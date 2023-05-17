3d73edc56b2a2ba0f8508478fe3ab76ec1cb9f71
xiongbo.yang <xiongbo.yang@roadefend.com>
2023-05-17 23:54:55 +0800

    init

--- src/components/Workspace/Header/index.tsx
+++ src/components/Workspace/Header/index.tsx
@@ -1,12 +1,12 @@
 import React, { useEffect, useState, useRef } from 'react'
 import styles from './index.css'
 import Workspace from '@/components/Workspace'
-import { Form, Input, Checkbox, Row, Col, Button, Modal, Menu, } from 'antd'
+import { Form, Input, Checkbox, Row, Col, Button, Modal, Menu, Space, Tooltip, Dropdown, Divider, } from 'antd'
 import type { MenuProps } from 'antd';
 import { RequestUtils, Utils, } from '../Utils'
 import type { DraggableData, DraggableEvent } from 'react-draggable';
 import Draggable from 'react-draggable';
-import { AppstoreOutlined, ExclamationCircleFilled, MailOutlined, SettingOutlined } from '@ant-design/icons'
+import { AppstoreOutlined, ExclamationCircleFilled, MailOutlined, SearchOutlined, SettingOutlined, SmileOutlined } from '@ant-design/icons'
 import axios from 'axios'
 import moment from 'moment'
 import CryptoJs from 'crypto-js'
@@ -23,69 +23,179 @@ const onClick: MenuProps['onClick'] = (e) => {
 
 const menuItems: MenuProps['items'] = [
   {
-    label: 'Navigation One',
-    key: 'mail',
-    icon: <MailOutlined />,
-  },
-  {
-    label: 'Navigation Two',
-    key: 'app',
-    icon: <AppstoreOutlined />,
-    disabled: true,
-  },
-  {
-    label: 'Navigation Three - Submenu',
-    key: 'SubMenu',
-    icon: <SettingOutlined />,
+    label: 'File',
+    key: 'File',
     children: [
       {
-        type: 'group',
-        label: 'Item 1',
-        children: [
-          {
-            label: 'Option 1',
-            key: 'setting:1',
-          },
-          {
-            label: 'Option 2',
-            key: 'setting:2',
-          },
-        ],
+        label: 'New',
+        key: 'New',
+        icon: <AppstoreOutlined />,
       },
       {
-        type: 'group',
-        label: 'Item 2',
-        children: [
-          {
-            label: 'Option 3',
-            key: 'setting:3',
-          },
-          {
-            label: 'Option 4',
-            key: 'setting:4',
-          },
-        ],
+        label: 'Open',
+        key: 'Open',
+      },
+      {
+        label: 'Save',
+        key: 'Save',
+      },
+      {
+        label: 'Save As ...',
+        key: 'SaveAs',
       },
     ],
   },
   {
-    label: (
-      <a href="https://ant.design" target="_blank" rel="noopener noreferrer">
-        Navigation Four - Link
-      </a>
-    ),
-    key: 'alipay',
+    label: 'Edit',
+    key: 'Edit',
+    children: [
+      {
+        label: 'New',
+        key: 'New',
+        icon: <AppstoreOutlined />,
+      },
+      {
+        label: 'Open',
+        key: 'Open',
+      },
+      {
+        label: 'Save',
+        key: 'Save',
+      },
+      {
+        label: 'Save As ...',
+        key: 'SaveAs',
+      },
+    ],
+  },
+  {
+    label: 'View',
+    key: 'View',
+    children: [
+      {
+        label: 'New',
+        key: 'New',
+        icon: <AppstoreOutlined />,
+      },
+      {
+        label: 'Open',
+        key: 'Open',
+      },
+      {
+        label: 'Save',
+        key: 'Save',
+      },
+      {
+        label: 'Save As ...',
+        key: 'SaveAs',
+      },
+    ],
+  },
+  {
+    label: 'Operation',
+    key: 'Operation',
+    children: [
+      {
+        label: 'New',
+        key: 'New',
+        icon: <AppstoreOutlined />,
+      },
+      {
+        label: 'Open',
+        key: 'Open',
+      },
+      {
+        label: 'Save',
+        key: 'Save',
+      },
+      {
+        label: 'Save As ...',
+        key: 'SaveAs',
+      },
+    ],
+  },
+  {
+    label: 'Options',
+    key: 'Options',
+    children: [
+      {
+        label: 'New',
+        key: 'New',
+        icon: <AppstoreOutlined />,
+      },
+      {
+        label: 'Open',
+        key: 'Open',
+      },
+      {
+        label: 'Save',
+        key: 'Save',
+      },
+      {
+        label: 'Save As ...',
+        key: 'SaveAs',
+      },
+    ],
+  },
+  {
+    label: 'Help',
+    key: 'Help',
+    children: [
+      {
+        label: 'New',
+        key: 'New',
+        icon: <AppstoreOutlined />,
+      },
+      {
+        label: 'Open',
+        key: 'Open',
+      },
+      {
+        label: 'Save',
+        key: 'Save',
+      },
+      {
+        label: 'Save As ...',
+        key: 'SaveAs',
+      },
+    ],
   },
 ]
+
+const items: MenuProps['items'] = [
+  {
+    key: '1',
+    label: (
+      <a target="_blank" rel="noopener noreferrer" href="https://www.antgroup.com">
+        1st menu item
+      </a>
+    ),
+  },
+  {
+    key: '2',
+    label: (
+      <a target="_blank" rel="noopener noreferrer" href="https://www.aliyun.com">
+        2nd menu item
+      </a>
+    ),
+  },
+  {
+    key: '3',
+    label: (
+      <a target="_blank" rel="noopener noreferrer" href="https://www.luohanacademy.com">
+        3rd menu item
+      </a>
+    ),
+  },
+];
 
 export default (props: any) => {
   const [initialized, setInitialized,] = useState<boolean>(false)
   const draggleRef = useRef<HTMLDivElement>(null);
   const [online, setOnline,] = useState<boolean>(false)
   const [userInfo, setUserInfo,] = useState<UserInfo | null>(null)
-  const [loginFormWindowVisible, setLoginFormWindowVisible, ] = useState<boolean>(false)
+  const [loginFormWindowVisible, setLoginFormWindowVisible,] = useState<boolean>(false)
 
-  let timer: any = null
 
   useEffect(() => {
     if (!initialized) {
@@ -95,12 +205,15 @@ export default (props: any) => {
 
   const initialize = () => {
     setInitialized(true)
-    timer = setInterval(async () => {
+    const timer = setInterval(async () => {
       const onlineResult = await RequestUtils.isOnline()
       setOnline(onlineResult)
       setUserInfo(RequestUtils.userInfo)
     }, 2000)
 
+    return () => {
+      clearInterval(timer)
+    }
   }
 
   const login = () => {
@@ -162,23 +275,66 @@ export default (props: any) => {
     RequestUtils.logout()
   }
 
+  //  <Menu mode='horizontal' items={menuItems} onClick={onClick} />
 
   return (
-    <div style={{ position: 'absolute', top: '0px', height: `${Utils.HEADER_HEIGHT}px`, width: '100%'}}>
-      <div style={{width: '100%', height: '50%', }}>
-        <div style={{ width: '100%', float: 'left' }}>
-          <Menu mode='horizontal' items={menuItems} onClick={onClick} />
+    <div style={{ position: 'absolute', top: '0px', height: `${Utils.HEADER_HEIGHT}px`, width: '100%' }}>
+      <div style={{ width: '100%', height: '50%', borderBottomStyle: 'inset', borderBottomWidth: '1px' }}>
+        <div style={{ width: '100%', height: '100%', float: 'left', display: 'table' }}>
+          <Space direction="horizontal" style={{ display: 'table-cell', verticalAlign: 'middle' }}>
+            <Space wrap>
+              <Dropdown menu={{ items }}>
+                <Button type='text'>File</Button>
+              </Dropdown>
+              <Dropdown menu={{ items }}>
+                <Button type='text'>Edit</Button>
+              </Dropdown>
+              <Dropdown menu={{ items }}>
+                <Button type='text'>View</Button>
+              </Dropdown>
+              <Dropdown menu={{ items }}>
+                <Button type='text'>Operation</Button>
+              </Dropdown>
+              <Dropdown menu={{ items }}>
+                <Button type='text'>Option</Button>
+              </Dropdown>
+              <Dropdown menu={{ items }}>
+                <Button type='text'>Help</Button>
+              </Dropdown>
+            </Space>
+          </Space>
         </div>
-        <div style={{ position: 'absolute', height: '50%', width: '90px', right: '0px', display:'table'}}>
-          <div style={{display: 'table-cell', verticalAlign: 'middle', }}>
-            {userInfo?.customerName} {online ? <Button type='primary' onClick={logout}>退出</Button> : <Button type='primary' onClick={login}>登录</Button>}
+        <div style={{ position: 'absolute', height: '50%', width: '240px', right: '0px' }}>
+          <div style={{ float: 'right', display: 'table', height: '100%', marginRight: '8px' }}>
+            <div style={{ display: 'table-cell', verticalAlign: 'middle' }}>
+              {online ? "Welcome " + userInfo?.customerName : ""} {online ? <Button type='primary' onClick={logout}>退出</Button> : <Button type='primary' onClick={login}>登录</Button>}
+            </div>
           </div>
         </div>
       </div>
-      <div style={{ width: '100%',height: '50%'  }}>
-        <Menu mode='horizontal' items={menuItems} onClick={onClick} />
+      <div style={{ width: '100%', height: '50%', }}>
+        <div style={{ float: 'left', height: '100%', display: 'table', marginLeft: '8px' }}>
+          <Space direction="horizontal" style={{ display: 'table-cell', verticalAlign: 'middle' }}>
+            <Space wrap>
+              <Dropdown menu={{ items }} placement="bottomLeft">
+                <Button>bottomLeft</Button>
+              </Dropdown>
+              <Tooltip title="search">
+                <Button shape="circle" icon={<SearchOutlined />} />
+              </Tooltip>
+              <Button icon={<SearchOutlined />}>Search</Button>
+              <Tooltip title="search">
+                <Button type="text" shape="circle" icon={<SearchOutlined />} />
+              </Tooltip>
+              <Button type="text" icon={<SearchOutlined />}>
+                Search
+              </Button>
+              <Button icon={<SearchOutlined />} href="https://www.google.com" />
+            </Space>
+          </Space>
+        </div>
       </div>
-      <LoginFormWindow visible={loginFormWindowVisible} x={60} y={60} onWindowCancel={handleLoginFormWindowCancel} onWindowOk={handleLoginFormWindowOk}/>
+      <LoginFormWindow visible={loginFormWindowVisible} x={60} y={60} onWindowCancel={handleLoginFormWindowCancel} onWindowOk={handleLoginFormWindowOk} />
     </div>
   )
 }
