4b53863f034a5d381ae59e9ef4273642b3bdb534
sean.yang <sean.yang@ivipa.com>
2023-08-28 17:19:50 +0800

    init

--- src/components/Workspace/Header/OpenFileWindow/index.tsx
+++ src/components/Workspace/Header/OpenFileWindow/index.tsx
@@ -1,20 +1,25 @@
 import React, { FC, useEffect, useState, useRef } from 'react'
 import styles from './index.css'
 import { Form, Input, Checkbox, Tree, Row, Col, Button, Modal, Menu, message, Alert, Space, } from 'antd'
-import { RequestUtils, Utils, } from '../../Utils'
+import { RequestUtils, SystemUtils, Utils, } from '../../Utils'
 import axios from 'axios'
 import Avatar from 'antd/lib/avatar/avatar'
-import { Document, Folder } from '../../Utils/RequestUtils'
+import { Document, Folder, isDocument, isFolder } from '../../Utils/RequestUtils'
 import type { DataNode, TreeProps, } from 'antd/es/tree';
 import { FileFilled, FileOutlined, FolderFilled, FolderOutlined } from '@ant-design/icons'
+import { StorageService } from '../../Storage'
 
 
 interface OpenFileWindowProps {
   visible: boolean;
   x: number;
   y: number;
+  disableFileName: boolean;
+  selectedFolderId: number | null;
+  selectedDocumentId: number | null;
+  selectedDocumentName: string;
   onWindowCancel: () => void;
-  onWindowOk: (documentId: number) => void
+  onWindowOk: (documentId: number, documentName: string | null, folderId: number | null) => void
 }
 
 const FOLDER = 'FOLDER_'
@@ -51,7 +56,7 @@ const treeData2: DataNode[] = [
 ];
 
 const OpenFileWindowPage: FC<OpenFileWindowProps> = ({
-  visible, x, y, onWindowCancel, onWindowOk,
+  visible, x, y, disableFileName, selectedFolderId, selectedDocumentId, selectedDocumentName, onWindowCancel, onWindowOk,
 }) => {
   const [dataLoading, setDataLoading,] = useState<boolean>(false)
   const [modalX, setModalX,] = useState<number>(0)
@@ -70,6 +75,13 @@ const OpenFileWindowPage: FC<OpenFileWindowProps> = ({
   const [selectedFolderKey, setSelectedFolderKey,] = useState<string>('')
   const [confirmDeleteFolderWindowVisible, setConfirmDeleteFolderWindowVisible,] = useState<boolean>(false)
   const [confirmDeleteDocumentWindowVisible, setConfirmDeleteDocumentWindowVisible,] = useState<boolean>(false)
+  const [selectedFolderIsFolder, setSelectedFolderIsFolder, ] = useState<boolean>(false)
+  const [selectedFolderIsDocument, setSelectedFolderIsDocument, ] = useState<boolean>(false)
+  const [confirmOverwriteWindowVisible, setConfirmOverwriteWindowVisible] = useState<boolean>(false)
+  const [confirmOverwriteInFolder, setConfirmOverwriteInFolder, ] = useState<boolean>(true)
+  const [confirmOverwriteFolderId, setConfirmOverwriteFolderId, ] = useState<number|null>(null)
+  const [confirmOverwriteDocumentId, setConfirmOverwriteDocumentId, ] = useState<number>(0)
+  const [confirmOverwriteDocumentName, setConfirmOverwriteDocumentName, ] = useState<string>('')
 
   if (origModalX != x) {
     setOrigModalX(x)
@@ -158,12 +170,15 @@ const OpenFileWindowPage: FC<OpenFileWindowProps> = ({
   }
 
   const onOk = () => {
-    //addFolderForm.submit()
-    if(selectedFolderKey?.length > 0) {
-      if(selectedFolderKey.startsWith(DOC)) {
-        let documentId = Number(selectedFolderKey.substring(DOC.length))
-        if(onWindowOk) {
-          onWindowOk(documentId)
+    if(disableFileName) { //Save file
+      handleSaveFile()
+    } else { // Open File
+      if(selectedFolderKey?.length > 0) {
+        if(selectedFolderKey.startsWith(DOC)) {
+          let documentId = Number(selectedFolderKey.substring(DOC.length))
+          if(onWindowOk) {
+            onWindowOk(documentId, null, null)
+          }
         }
       }
     }
@@ -199,10 +214,19 @@ const OpenFileWindowPage: FC<OpenFileWindowProps> = ({
 
   const onFolderSelect: TreeProps['onSelect'] = (selectedKeys, info) => {
     console.log('selected', selectedKeys, info);
+    setSelectedFolderIsDocument(false)
+    setSelectedFolderIsFolder(false)
     if(selectedKeys.length > 0) {
-      setSelectedFolderKey(selectedKeys[0].toString())
+      let selectedKey = selectedKeys[0].toString()
+      setSelectedFolderKey(selectedKey)
+      if(selectedKey.startsWith(FOLDER)) {
+        setSelectedFolderIsFolder(true)        
+      }
+      if(selectedKey.startsWith(DOC)) {
+        setSelectedFolderIsDocument(true)        
+      }
     } else {
-      setSelectedFolderKey("");
+      setSelectedFolderKey("");      
     }
   }
 
@@ -219,6 +243,7 @@ const OpenFileWindowPage: FC<OpenFileWindowProps> = ({
             alert('Failed to delete folder.')
           }
           setConfirmDeleteFolderWindowVisible(false)
+          fetchData()
         }
         fetchDeleteFolderData()
       }
@@ -240,6 +265,7 @@ const OpenFileWindowPage: FC<OpenFileWindowProps> = ({
             alert('Failed to delete document.')
           }
           setConfirmDeleteDocumentWindowVisible(false)
+          fetchData()
         }
         fetchDeleteDocumentData()
       }
@@ -281,27 +307,153 @@ const OpenFileWindowPage: FC<OpenFileWindowProps> = ({
     }
     fetchFolderData()
   }
+
+  const confirmOverwriteDocument = () => {
+    setConfirmOverwriteWindowVisible(false)
+    if(confirmOverwriteInFolder) {
+      doSaveFile(confirmOverwriteFolderId, true, confirmOverwriteDocumentId, confirmOverwriteDocumentName)
+    } else {
+      doSaveFile(confirmOverwriteFolderId, true, confirmOverwriteDocumentId, confirmOverwriteDocumentName)
+    }
+  }
+
+  const cancelOverwriteDocument = () => {
+    setConfirmOverwriteWindowVisible(false)
+  }
+
+  const handleSaveFile = () => {
+    let folderId: number | null = null
+    let documentId: number | null = null
+    let saveInFolder = true
+    if (selectedFolderKey?.startsWith(FOLDER)) {
+      folderId = parseInt(selectedFolderKey.substring(7))     
+      setConfirmOverwriteFolderId(folderId) 
+    } else if (selectedFolderKey?.startsWith(DOC)) {
+      documentId = parseInt(selectedFolderKey.substring(4))
+      setConfirmOverwriteDocumentId(documentId)
+      saveInFolder = false      
+    } else {
+      SystemUtils.handleInternalError(`Unknow error occurs here with selectedFolderKey=${selectedFolderKey}`)
+      return
+    }
+
+    if(saveInFolder) {
+      let folderMap = treeMap?.get(selectedFolderKey);
+      let hasDocumentName = false
+      let currentDocumentId : number = 0
+      let currentDocumentName: string = ''
+      if(isFolder(folderMap)) {
+        folderMap.data.children?.forEach(child => {
+          if(child.title == selectedDocumentName) {
+            hasDocumentName = true
+            let document =  treeMap?.get('' + child.key)
+            if(isDocument(document)) {
+              currentDocumentId = document.documentId
+              currentDocumentName = document.documentName
+            }
+          }
+        })
+      }
+      if(selectedDocumentId == null) {
+        if(hasDocumentName) {
+          setConfirmOverwriteWindowVisible(true)
+          setConfirmOverwriteInFolder(true)
+          setConfirmOverwriteDocumentId(currentDocumentId)
+          setConfirmOverwriteDocumentName(currentDocumentName)
+        } else {
+          doSaveFile(folderId, false, 0, '')
+        }
+      } else {
+        if(hasDocumentName) {
+          setConfirmOverwriteWindowVisible(true)
+          setConfirmOverwriteInFolder(true)
+          setConfirmOverwriteDocumentId(currentDocumentId)
+          setConfirmOverwriteDocumentName(currentDocumentName)
+        } else {
+          doSaveFile(folderId, false, 0, '')
+        }
+      }
+    } else {
+     let documentData = treeMap?.get('' + documentId); 
+     if(isDocument(documentData)) {
+      if(selectedDocumentName == documentData.documentName && selectedDocumentId == documentData.documentId) {
+        doSaveFile(folderId, true, documentData.documentId, documentData.documentName)
+      } else {
+        setConfirmOverwriteWindowVisible(true)
+        setConfirmOverwriteInFolder(false)
+        setConfirmOverwriteFolderId(documentData.folderId)
+        setConfirmOverwriteDocumentId(documentData.documentId)
+        setConfirmOverwriteDocumentName(documentData.documentName)
+      }
+     } else {
+      SystemUtils.handleInternalError(`Unknown documentData, documentId = ${documentId}`)
+     }
+    }
+  }
+
+  const doSaveFile = (folderId: number | null, overwrite: boolean, documentId: number, newDocumentName: string) => {
+    const storage = new StorageService()
+    storage.editors = Utils.editors
+    storage.save()
+    const documentData = storage.storageData
+    const documentContent = JSON.stringify(documentData)
+    console.log(documentContent)
+    let resultDocumentId = documentId
+    let resultDocumentName = newDocumentName
+    const saveDocumentData = async () => {
+      let documentData = null
+      if(overwrite) {
+        documentData = await RequestUtils.updateDocument(documentId, newDocumentName, documentContent, folderId)
+      } else {
+        documentData = await RequestUtils.saveDocument(selectedDocumentName, documentContent, folderId)
+      }
+      if (documentData.data?.success) {
+        console.log('Save document wwith data: ', documentData.data.data)
+        if(!overwrite) { //New Document
+          resultDocumentId = documentData.data.data.documentId
+        } else {
+          resultDocumentName = selectedDocumentName
+        }
+        Utils.editors.forEach(editor => {
+          editor.resetModified()
+        })
+        setErrorMessage('')
+        setErrorVisible(false)
+        if(onWindowOk) {
+          onWindowOk(resultDocumentId, selectedDocumentName, folderId)
+        }
+      } else {
+        console.log('Save document with error: ', documentData.data)
+        setErrorMessage(documentData.data.message)
+        setErrorVisible(true)
+      }
+    }
+    saveDocumentData()
+  }
+
   return (
     <div>
       <Modal title="Open File" centered open={visible} onOk={onOk} onCancel={onCancel} maskClosable={false}  >
         <div style={{ width: '100%', height: '480px' }}>
           <Space wrap>
             <Button onClick={openAddFolder}>Add Folder</Button>
-            <Button onClick={handleDeleteFolder}>Delete Folder</Button>
-            <Button onClick={handleDeleteDocument}>Delete Document</Button>
+            <Button onClick={handleDeleteFolder} disabled={!selectedFolderIsFolder}>Delete Folder</Button>
+            <Button onClick={handleDeleteDocument} disabled={!selectedFolderIsDocument}>Delete Document</Button>
           </Space>
-          <Tree style={{ width: '100%', height: '100%', overflow: 'scroll', margin: '8px' }}
-            height={420}
-            showLine showIcon
-            //checkable
-            selectable
-            //defaultExpandedKeys={['0-0-0', '0-0-1']}
-            //defaultSelectedKeys={['0-0-0', '0-0-1']}
-            //defaultCheckedKeys={['0-0-0', '0-0-1']}
-            onSelect={onFolderSelect}
-            //onCheck={onCheck}
-            treeData={treeData}
-          />
+          <div style={{ width: '100%', height: '440px',borderWidth: '1px', borderColor: 'silver', borderStyle: 'solid', marginTop: '8px' }}>
+            <Tree style={{ width: '100%', height: '100%', overflow: 'scroll', margin: '8px', }}
+              height={420}
+              showLine showIcon
+              //checkable
+              selectable
+              //defaultExpandedKeys={['0-0-0', '0-0-1']}
+              //defaultSelectedKeys={['0-0-0', '0-0-1']}
+              //defaultCheckedKeys={['0-0-0', '0-0-1']}
+              onSelect={onFolderSelect}
+              //onCheck={onCheck}
+              treeData={treeData}
+            />
+          </div>
         </div>
       </Modal>
       <Modal title="Modal" centered open={addFolderWindowVisible} onOk={confirmAddFolder} onCancel={cancelAddFolder} okText="确认" cancelText="取消" >
@@ -312,6 +464,9 @@ const OpenFileWindowPage: FC<OpenFileWindowProps> = ({
           </Form.Item>
           {errorVisible ? <Alert message={errorMessage} type="error" showIcon/> : ''}          
           </Form>
+      </Modal>
+      <Modal title="Modal" centered open={confirmOverwriteWindowVisible} onOk={confirmOverwriteDocument} onCancel={cancelOverwriteDocument} okText="确认" cancelText="取消" >
+        <p>File already exists, are you sure to overwrite it?</p>
       </Modal>
       <Modal title="Modal" centered open={confirmDeleteFolderWindowVisible} onOk={confirmDeleteFolder} onCancel={cancelDeleteFolder} okText="确认" cancelText="取消" >
         <p>Are you sure to delete the folder?</p>
