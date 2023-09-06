bb2e13f44ae477a037c60ae4acf01ef10c5835bc
xiongbo.yang <xiongbo.yang@roadefend.com>
2023-09-06 20:47:25 +0800

    init

--- src/components/Rockie/Actions/src/ShapeAction.ts
+++ src/components/Rockie/Actions/src/ShapeAction.ts
@@ -2,11 +2,24 @@ import { Paint, } from '@/components/Engine'
 import { Theme, } from '@/components/Workspace/Theme'
 import { Item, ShapeEntity, Shapes, } from '../../Items'
 import { Action, } from './Action'
+import { ShapeTypes } from '../../Items/src/ShapeEntity'
 
 export class ShapeAction extends Action {
 
   protected buildItem(): Item {
-    const shapeEntity = new ShapeEntity(250, 250, 200, 200, {
+    let left = 0
+    let top = 0
+    let width = 100
+    let height = 100
+    ShapeTypes.forEach(shapeType => {
+      if(shapeType.name == this.type) {
+        left = shapeType.left
+        top = shapeType.top
+        width = shapeType.width
+        height = shapeType.height
+      }
+    })
+    const shapeEntity = new ShapeEntity(left, top, width, height, {
       shapeType: this.type ? this.type : Shapes.TYPE_RECTANGLE,
       freezetype: this.freezeType ? this.freezeType : Shapes.FREEZE_NONE
     })
