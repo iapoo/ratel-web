bfa25db640b101d2f9b32d65883a544b231c2d1b
sean.yang <sean.yang@ivipa.com>
2024-03-29 15:53:16 +0800

    Web Font check

--- src/components/Engine/src/EngineUtils.ts
+++ src/components/Engine/src/EngineUtils.ts
@@ -1,5 +1,27 @@
 import axios, { AxiosResponse, AxiosRequestConfig, } from 'axios'
 
+
+export class EngineUtils {
+  public static FONT_NAME_DEFAULT = 'NotoSerifSC'
+  //public static FONT_NAME_SANS = 'Sans'
+  //public static FONT_NAME_SERIF = 'Serif'
+  public static FONT_NAME_ROBOTO = 'Roboto'
+  public static FONT_NAME_NOTOSERIFSC = 'NotoSerifSC'
+  public static FONT_SIZE_DEFAULT = 14
+
+  public static LANG_EN_US = 'en-US'
+  public static LANG_ZN_CN = 'zh-CN'
+
+  public static getCanvasKitWasm = () => {
+    return axios.get(`/resources/canvaskit.wasm`, {
+      headers: {
+        'Content-Type': 'application/wasm',
+      },
+      responseType: 'arraybuffer',
+    })
+  }
+}
+
 export enum WebFontStatus {
   NOT_LOADED,
   LOADING,
@@ -33,25 +55,47 @@ export interface LanguageFont {
 }
 
 export const LanguageFonts = [
-  {language: 'en-US', defaultLatinFont: 'Roboto', defaultNonLatinFont: 'NotoSerifSC', fonts: ['Roboto', 'Lato', 'Open Sans', 'Source Sans Pro', 'Roboto Slab',]},
-  {language: 'zh-CN', defaultLatinFont: 'Roboto', defaultNonLatinFont: 'NotoSerifSC', fonts: ['Roboto', 'Noto Serif SC', 'Noto Sans SC']},
+  {language: EngineUtils.LANG_EN_US, defaultLatinFont: EngineUtils.FONT_NAME_ROBOTO, defaultNonLatinFont: EngineUtils.FONT_NAME_NOTOSERIFSC, 
+    fonts: ['Lato', 'Open Sans', 'Source Sans Pro', 'Roboto Slab',]},
+  {language: EngineUtils.LANG_ZN_CN, defaultLatinFont: EngineUtils.FONT_NAME_ROBOTO, defaultNonLatinFont: EngineUtils.FONT_NAME_NOTOSERIFSC, 
+    fonts: ['Noto Sans SC']},
 ]
 
 export const SystemFonts = [
-  {fontName: 'Roboto', fontUrl: '/fonts/Roboto-Regular.woff2'},
-  {fontName: 'NotoSansSC', fontUrl: '/fonts/Roboto-Regular.woff2'},
-  {fontName: 'NotoSerifSC', fontUrl: '/fonts/Roboto-Regular.woff2'},
+  {fontName: EngineUtils.FONT_NAME_ROBOTO, fontUrl: '/fonts/Roboto-Regular.woff2'},
+  {fontName: EngineUtils.FONT_NAME_NOTOSERIFSC, fontUrl: '/fonts/Noto-Serif-SC-Regular.woff2'},
 ]
 
-export const MotoSerifSCStyleText = `
-`
+export enum WebFontSource {
+  GOOGLE,  
+}
 
-export class WebFonts {  
+export interface WebFontMeta {
+  fontFamily: string,
+  source: WebFontSource
+}
+
+export const WebFonts: WebFontMeta[] = [
+  {fontFamily: 'Lato', source: WebFontSource.GOOGLE, },
+  {fontFamily: 'Lora', source: WebFontSource.GOOGLE, },
+  {fontFamily: 'Merriweather', source: WebFontSource.GOOGLE, },
+  {fontFamily: 'Montserrat', source: WebFontSource.GOOGLE, },
+  {fontFamily: 'Noto Sans', source: WebFontSource.GOOGLE, },
+  {fontFamily: 'Noto Sans SC', source: WebFontSource.GOOGLE, },
+  {fontFamily: 'Noto Serif', source: WebFontSource.GOOGLE, },
+  {fontFamily: 'Noto Serif SC', source: WebFontSource.GOOGLE, },
+  {fontFamily: 'Open Sans', source: WebFontSource.GOOGLE, },
+  {fontFamily: 'Oswald', source: WebFontSource.GOOGLE, },
+  {fontFamily: 'PT Sans', source: WebFontSource.GOOGLE, },
+  {fontFamily: 'PT Serif', source: WebFontSource.GOOGLE, },
+  {fontFamily: 'Raleway', source: WebFontSource.GOOGLE, },
+  {fontFamily: 'Source Sans Pro', source: WebFontSource.GOOGLE, },
+  {fontFamily: 'Roboto Mono', source: WebFontSource.GOOGLE, },
+  {fontFamily: 'Roboto Slab', source: WebFontSource.GOOGLE, },
+]
+
+export class WebFontManager {  
   private _webFonts: Map<string, WebFont[]> = new Map<string, WebFont[]>()
-  
-  public initialize() {
-
-  }
 
   public registWebFont(key: string, webFont: WebFont) {    
     let fonts = this._webFonts.get(key)
@@ -71,36 +115,42 @@ export class WebFonts {
   }
 
   public getWebFontKeys() {
-    return this._webFonts.keys
+    return this._webFonts.keys()
   }
 
-  public getWebFont(key: string) {
+  public getWebFonts(key: string) {
     return this._webFonts.get(key)
   }
 
   private async registSystemFont(fontName: string, fontUrl: string) {
     const webFont = new WebFont()
     const response = await fetch(fontUrl)
-    const fontData = await response.arrayBuffer()
+    const fontData = await response.arrayBuffer()    
   }
 }
 
-export class EngineUtils {
-  public static FONT_NAME_DEFAULT = 'NotoSerifSC'
-  //public static FONT_NAME_SANS = 'Sans'
-  //public static FONT_NAME_SERIF = 'Serif'
-  public static FONT_NAME_ROBOTO = 'Roboto'
-  public static FONT_NAME_NOTOSERIFSC = 'NotoSerifSC'
-  public static FONT_SIZE_DEFAULT = 14
-  public static webFonts: WebFonts = new WebFonts()
-  
-  public static getCanvasKitWasm = () => {
-    return axios.get(`/resources/canvaskit.wasm`, {
-      headers: {
-        'Content-Type': 'application/wasm',
-      },
-      responseType: 'arraybuffer',
-    })
+export class FontUtils {
+  public static webFontManager = new WebFontManager()
+
+  public static async loadWebFontFiles() {
+    const webFontKeys = FontUtils.webFontManager.getWebFontKeys()
+    for(let key of webFontKeys) {
+      const webFonts = FontUtils.webFontManager.getWebFonts(key)
+      if(webFonts && webFonts.length > 0) {
+        for(let webFont of webFonts) {
+          const webFontFile = await FontUtils.getWebFontFile(webFont.url)
+          if(webFontFile.data) {
+            console.log(webFontFile.data.byteLength)
+            break;
+          }
+        }
+      }
+    }
+  }
+
+  public static async getWebFontFile(url: string) {
+    const webFontFile = await axios.get<ArrayBuffer, AxiosResponse<ArrayBuffer>>(url, {responseType: 'arraybuffer'})
+    return webFontFile
   }
 
   public static getWebFontStyleSheet(familyName: string) {
@@ -108,7 +158,7 @@ export class EngineUtils {
   }
 
   public static async getWebFontData(familyName: string) {
-    const fontStyleSheet = await EngineUtils.getWebFontStyleSheet(familyName)
+    const fontStyleSheet = await FontUtils.getWebFontStyleSheet(familyName)
     //console.log(`${fontStyleSheet}`)
     if (fontStyleSheet.status == 200) {
       const style = document.createElement('style')
@@ -136,7 +186,7 @@ export class EngineUtils {
                   webFont.src = src
                   webFont.fontStyle = fontStyle
                   webFont.fontWeight = fontWeight
-                  EngineUtils.webFonts.registWebFont(fontFamily, webFont)
+                  FontUtils.webFontManager.registWebFont(fontFamily, webFont)
 
                   const urlRegexp = /url\((\"https\:\/\/fonts\.gstatic\.com\/.*)\) format/gm
                   const urlMatch = urlRegexp.exec(src)
