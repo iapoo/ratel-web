3bdefe7801bd854311a7e04fc0d85ca5ed456693
xiongbo.yang <xiongbo.yang@roadefend.com>
2024-04-24 17:27:39 +0800

    Add new Package item for UML

--- src/components/Rockie/CustomItems/UML/index.ts
+++ src/components/Rockie/CustomItems/UML/index.ts
@@ -1,9 +1,10 @@
+import { UMLContainerShape, UMLContainerTypes } from './src/UMLContainerShape'
 import { UMLGridShape, UMLGridShapeTypes, } from './src/UMLGridShape'
 
 export  {UMLGridShapeTypes, UMLGridShape} from './src/UMLGridShape'
 
 
-export const UMLShapes = [
+export const UMLGridShapes = [
     {name: UMLGridShapeTypes[0].name, type: UMLGridShape, typeInfo:  UMLGridShapeTypes[0] },
     {name: UMLGridShapeTypes[1].name, type: UMLGridShape, typeInfo:  UMLGridShapeTypes[1] },
     {name: UMLGridShapeTypes[2].name, type: UMLGridShape, typeInfo:  UMLGridShapeTypes[2] },
@@ -16,4 +17,8 @@ export const UMLShapes = [
     {name: UMLGridShapeTypes[9].name, type: UMLGridShape, typeInfo:  UMLGridShapeTypes[9] },
     {name: UMLGridShapeTypes[10].name, type: UMLGridShape, typeInfo:  UMLGridShapeTypes[10] },
     {name: UMLGridShapeTypes[11].name, type: UMLGridShape, typeInfo:  UMLGridShapeTypes[11] },
+]
+
+export const UMLContainerShapes = [
+    {name: UMLContainerTypes[0].name, type: UMLContainerShape, typeInfo:  UMLContainerTypes[0] },
 ]
--- src/components/Rockie/CustomItems/UML/src/UMLContainerShape.ts
+++ src/components/Rockie/CustomItems/UML/src/UMLContainerShape.ts
@@ -2,46 +2,45 @@ import { FontSlant, FontWeight, Graphics, ParagraphDirection, Point2, Rectangle,
 import { EntityShapeType } from '../../../Shapes/src/EntityShape'
 import { CustomShape } from '../../../Shapes'
 import { ContainerEntity, CustomEntity, CustomTableEntity, Shapes, TableEntity } from '../../../Items'
-import { Type } from '../../../Items/src/Item'
-import { ShapeOptions } from '@/components/Rockie/Items/src/ShapeEntity'
+import { Categories, Type } from '../../../Items/src/Item'
+import { ShapeOptions, ShapeType } from '@/components/Rockie/Items/src/ShapeEntity'
 
 
 export class UMLContainers {
-  public static TYPE_HORIZONTAL_CONTAINER = 'Horizontal Container'
-  public static TYPE_VERTICAL_CONTAINER = 'Vertical Container'
+  public static TYPE_PACKAGE = 'Package'
 
-  public static DESC_HORIZONTAL_CONTAINER = 'Horizontal Container'
-  public static DESC_VERTICAL_CONTAINER = 'Vertical Container'
-
-  public static FREEZE_NONE = 'None'
-  public static FREEZE_WIDTH = 'Width'
-  public static FREEZE_WIDTH_HEIGHT =  'WidthHeight'
-  public static FREEZE_HEIGHT = 'Height'
-  public static FREEZE_ASPECT_RATIO = 'AspectRatio'
-
-  public static ADAPTER_DIRECTION_X = 'X'
-  public static ADAPTER_DIRECTION_Y = 'Y'
-
+  public static DESC_PACKAGE = 'Package'
 }
 
 export const UMLContainerTypes = [
-  { name: UMLContainers.TYPE_HORIZONTAL_CONTAINER, description: UMLContainers.DESC_HORIZONTAL_CONTAINER, freeze: UMLContainers.FREEZE_NONE, text: 'Horizontal Container', left: 0, top: 0, width: 200, height: 200, enableMask: false, 
-    modifiable: true, modifierX: 32, modifierY: 0,  modifierStartX: 0, modifierStartY: 0, modifierEndX: 0.5, modifierEndY: 0, modifyInLine: true, modifyInPercent: false,
-    controllable: true, controllerX: 0, controllerY: 1, controllerStartX: 0.05, controllerStartY: 0, controllerEndX: 0.05, controllerEndY: 1, controlInLine: true, controlInPercent: true,
-    adaptable: false, adapterX: 0, adapterY: 0,adapterDirection: 'X', adapterSize: 0, adapterStartX: 0, adapterStartY: 0, adapterEndX: 0, adapterEndY: 0, adaptInLine: true, adaptInPercent: true  },
-  { name: UMLContainers.TYPE_VERTICAL_CONTAINER, description: UMLContainers.DESC_VERTICAL_CONTAINER, freeze: UMLContainers.FREEZE_NONE, text: 'Vertical Container', left: 0, top: 0, width: 200, height: 200, enableMask: false, 
+  { name: UMLContainers.TYPE_PACKAGE, description: UMLContainers.DESC_PACKAGE, freeze: Shapes.FREEZE_NONE, text: 'Package', left: 0, top: 0, width: 200, height: 200, enableMask: false, 
     modifiable: true, modifierX: 0, modifierY: 32,  modifierStartX: 0, modifierStartY: 0, modifierEndX: 0, modifierEndY: 0.5, modifyInLine: true, modifyInPercent: false,
-    controllable: true, controllerX: 1, controllerY: 0, controllerStartX: 0, controllerStartY: 0.05, controllerEndX: 1, controllerEndY: 0.05, controlInLine: true, controlInPercent: true,
+    controllable: true, controllerX: 90, controllerY: 0, controllerStartX: 0, controllerStartY: 0.05, controllerEndX: 1, controllerEndY: 0.05, controlInLine: true, controlInPercent: false,
     adaptable: false, adapterX: 0, adapterY: 0,adapterDirection: 'X', adapterSize: 0, adapterStartX: 0, adapterStartY: 0, adapterEndX: 0, adapterEndY: 0, adaptInLine: true, adaptInPercent: true  },
 ]
-/**
- * TODO： Not implemented yet
- */
-export class UMLContainerEntity extends ContainerEntity {
 
-    public constructor(left: number, top: number, width: number, height: number, shapeOptions: ShapeOptions = { shapeType: UMLContainers.TYPE_VERTICAL_CONTAINER }) {
-      super(left, top, width, height, shapeOptions)
-      this.shape.controller = new Point2(0.4, 0)
+export class UMLContainerShape extends ContainerEntity {
+
+    public constructor(left: number, top: number, width: number, height: number, shapeOptions: ShapeOptions = { shapeType: UMLContainers.TYPE_PACKAGE }, shapeTypes: ShapeType[] = UMLContainerTypes) {
+      super(left, top, width, height, shapeOptions, shapeTypes)
     }
+
+    public get types(): Type[] {
+      return UMLContainerTypes
+    }
+
+    public get category(): string {
+      return Categories.CONTAINER
+    }   
+
+    protected parseEntityShapeType(type: string): EntityShapeType {
+      let shapeType = EntityShapeType.Rectangle
+      switch (type) {
+        case UMLContainers.TYPE_PACKAGE:
+          shapeType = EntityShapeType.VerticalContainer
+          break;
+      }
+      return shapeType
+    } 
 
 }
