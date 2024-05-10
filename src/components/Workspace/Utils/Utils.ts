6d64cef22d683ba40ab1a36eccff671a4a7d9507
sean.yang <sean.yang@ivipa.com>
2024-05-10 15:47:56 +0800

    Optimize navigator width

--- src/components/Workspace/Utils/Utils.ts
+++ src/components/Workspace/Utils/Utils.ts
@@ -5,7 +5,7 @@ export class Utils {
     public static HEADER_HEIGHT = 76
     public static FOOTER_HEIGHT = 0
     public static TITLE_HEIGHT = 56
-    public static DEFAULT_NAVIGATOR_WIDTH = 204
+    public static DEFAULT_NAVIGATOR_WIDTH = 218
     public static MIN_NAVIGATOR_WIDTH = 32
     public static MAX_NAVIGATOR_WIDTH = 480
     public static DEFAULT_DIVIDER_WIDTH = 6    
