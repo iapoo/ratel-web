dfb2f79c8d0dbcf88e936a5d363401956d230750
iabro“ <iabro@dev>
2024-07-16 15:11:58 +0800

    For electron builder

--- app/main.js
+++ app/main.js
@@ -5,7 +5,7 @@ const createWindow = () => {
   const win = new BrowserWindow({
     width: 1000,
     height: 800,
-    icon: `${__dirname}/static/favicon.ico`, // 设置窗口左上角的图标
+    icon: `static/favicon.ico`, // 设置窗口左上角的图标
     webPreferences: {
       nodeIntegration: true,
       contextIsolation: false,
@@ -13,6 +13,6 @@ const createWindow = () => {
     },
   });
   //win.webContents.openDevTools();
-  win.loadFile(`${__dirname}/dist/index.html`); //本地地址
+  win.loadFile(`dist/index.html`); //本地地址
 };
 app.whenReady().then(createWindow);
