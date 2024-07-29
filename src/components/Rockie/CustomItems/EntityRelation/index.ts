c945d96b2df7fa29330c56ed075ebe7ff3837f9b
xiongbo.yang <xiongbo.yang@roadefend.com>
2024-07-29 15:56:00 +0800

    For E-R shapes

--- src/components/Rockie/CustomItems/EntityRelation/index.ts
+++ src/components/Rockie/CustomItems/EntityRelation/index.ts
@@ -8,4 +8,6 @@ export const ERCustomShapes = [
     { name: ERCustomShapeTypes[3].name, type: ERCustomShape, typeInfo: ERCustomShapeTypes[3] },
     { name: ERCustomShapeTypes[4].name, type: ERCustomShape, typeInfo: ERCustomShapeTypes[4] },
     { name: ERCustomShapeTypes[5].name, type: ERCustomShape, typeInfo: ERCustomShapeTypes[5] },
+    { name: ERCustomShapeTypes[6].name, type: ERCustomShape, typeInfo: ERCustomShapeTypes[6] },
+    { name: ERCustomShapeTypes[7].name, type: ERCustomShape, typeInfo: ERCustomShapeTypes[7] },
 ]
