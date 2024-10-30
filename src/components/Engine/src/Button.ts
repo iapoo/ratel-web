01629ecd36f55df55f3d821e43fa5ed7f51e7e7d
sean.yang <sean.yang@ivipa.com>
2024-10-30 19:42:11 +0800

    Feat: code format

--- src/components/Engine/src/Button.ts
+++ src/components/Engine/src/Button.ts
@@ -1,25 +1,26 @@
 /* eslint-disable max-params */
 /* eslint-disable @typescript-eslint/explicit-member-accessibility */
-import { Colors, Graphics, Paint, PaintStyle, Rectangle, PointerEvent, } from './Graphics'
-import { Control, } from './Control'
+import { Control } from './Control';
+import { Colors, PointerEvent } from './Graphics';
 
 export class Button extends Control {
   constructor(left = 0, top = 0, width = 100, height = 100, text = 'Button') {
-    super(left, top, width, height, text)
-    this.onPointerEnter(this.handleOnPainterEnter.bind(this))
-    this.onPointerLeave(this.handleOnPainterLeave.bind(this))
-    this.onPointerClick(this.handleOnPainterClick.bind(this))
+    super(left, top, width, height, text);
+    this.onPointerEnter(this.handleOnPainterEnter.bind(this));
+    this.onPointerLeave(this.handleOnPainterLeave.bind(this));
+    this.onPointerClick(this.handleOnPainterClick.bind(this));
   }
 
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   private handleOnPainterEnter(e: PointerEvent): void {
-    this.fill.setColor(Colors.AliceBlue)
+    this.fill.setColor(Colors.AliceBlue);
   }
 
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   private handleOnPainterLeave(e: PointerEvent): void {
-    this.fill.setColor(Colors.LightGrey)
+    this.fill.setColor(Colors.LightGrey);
   }
 
-  private handleOnPainterClick(e: PointerEvent): void {
-
-  }
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  private handleOnPainterClick(e: PointerEvent): void {}
 }
