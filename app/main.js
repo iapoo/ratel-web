1d0bd773594d263a3eab3ad883396e30cc55aecc
sean.yang <sean.yang@ivipa.com>
2024-11-15 21:43:29 +0800

    Feat: Make header draggable in electron

--- app/main.js
+++ app/main.js
@@ -1,33 +1,32 @@
-const { app, BrowserWindow, Menu } = require('electron');
-const os = require('os');
-const path = require('path');
+const { app, BrowserWindow, Menu } = require('electron')
+const os = require('os')
+const path = require('path')
 
-const platform = os.platform();
+const platform = os.platform()
 const createWindow = () => {
-  Menu.setApplicationMenu(null); // null值取消顶部菜单栏
+  Menu.setApplicationMenu(null) // null值取消顶部菜单栏
   const win = new BrowserWindow({
     width: 1000,
     height: 800,
     icon: `static/favicon.ico`, // 设置窗口左上角的图标
     //frame: false,
-    titleBarStyle:
-      platform === 'darwin' || platform === 'win32' ? 'hidden' : undefined,
+    titleBarStyle: platform === 'darwin' || platform === 'win32' ? 'hidden' : undefined,
     trafficLightPosition: platform === 'darwin' ? { x: 10, y: 12 } : undefined,
     titleBarOverlay:
       platform === 'win32'
         ? {
-          color: 'rgba(0,0,0,0)',
-          height: 38,
-          symbolColor: 'darkgray',
-        }
+            color: 'rgba(0,0,0,0)',
+            height: 38,
+            symbolColor: 'darkgray',
+          }
         : undefined,
     webPreferences: {
       nodeIntegration: true,
       contextIsolation: false,
       // preload: path.join(__dirname, 'preload.js')
     },
-  });
-  //win.webContents.openDevTools();
-  win.loadFile(`dist/index.html`); //本地地址
-};
-app.whenReady().then(createWindow);
+  })
+  //win.webContents.openDevTools()
+  win.loadFile(`dist/index.html`) //本地地址
+}
+app.whenReady().then(createWindow)
